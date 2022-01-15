d0a915f0fea57ce6b3897ababc3b5013767eb7cd
SkBull(Win10) <31233236+SkBull@users.noreply.github.com>
2022-01-16 01:49:14 +0900

    fix main.ts

--- src/main.ts
+++ src/main.ts
@@ -24,8 +24,6 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
-createApp(App).use(router).mount('#app');
-
 const app = createApp(App)
   .use(IonicVue)
   .use(router);
