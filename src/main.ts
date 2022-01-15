c766c6f801b554012bfc6533474eca2955eaeb1a
SkBull(Win10) <31233236+SkBull@users.noreply.github.com>
2022-01-16 01:30:06 +0900

    first

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
+import './registerServiceWorker';
 
 import { IonicVue } from '@ionic/vue';
 
@@ -22,6 +23,8 @@ import '@ionic/vue/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+
+createApp(App).use(router).mount('#app');
 
 const app = createApp(App)
   .use(IonicVue)
